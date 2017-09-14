884ee7d3eeacb93b3c14313221c2e6029a5c5431
enkaizer <caique1994_1@hotmail.com>
2017-09-14 14:31:23 -0300

    selectReducer

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -3,13 +3,33 @@
  */
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {ListView} from 'react-native';
+import ListItem from './ListItem';
 
 class LibraryList extends Component {
+
+	componentWillMount = () => {
+		const ds = new ListView.DataSource({
+			rowHasChanged: (r1, r2) => r1 !== r2
+		})
+
+		this.DataSource = ds.cloneWithRows(this.props.LibraryList);
+	}
+
+	renderRow = (library) => {
+		return <ListItem text={library}/>
+	}
 
     render(){
         console.log(this.props);
 
-        return;
+        return (
+
+        	<ListView
+        		dataSource={this.DataSource}
+        		renderRow={this.renderRow}
+        	/>
+        )
     }
 }
 
